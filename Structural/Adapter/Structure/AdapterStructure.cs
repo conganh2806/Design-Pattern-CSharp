bc7ffae3049241966837a50e8a2c81c391fd4609
AzureAD\AnhNguyenCong <nashtech_anh.nguyencong@privalgo.co.uk>
2026-01-05 23:42:13 +0700

    Add structure adapter pattern

---
+++ Structural/Adapter/Structure/AdapterStructure.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Structural.Adapter.Structure
+{
+    public class AdapterStructure
+    {
+        public static void Main(string[] args)
+        {
+            ITarget target = new Adapter();
+            target.Request();
+        }
+    }
+
+    public interface ITarget
+    {
+        public void Request();
+    }
+
+    public class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter()
+        {
+            _adaptee = new Adaptee();
+        }
+
+        public void Request()
+        {
+            _adaptee.SpecificRequest();
+        }
+    }
+
+    public class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called Adaptee SpecificRequest");
+        }
+    }
+}
